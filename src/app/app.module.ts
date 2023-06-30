4df00c33cdd51d2e1e3995fe61163a4264e1579f
Lucas <lucas.silva@kardbank.com.br>
2023-06-29 23:54:06 -0300

    comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
+import {MatIconModule} from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { TesteComponent } from './teste/teste.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuTopComponent } from './components/menu-top/menu-top.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TesteComponent,
-    MenuComponent
+    MenuComponent,
+    MenuTopComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent{
   isOpen = false;
 
+  constructor(private router: Router) {}
+  isCurrentPage(route: string): boolean {
+    return this.router.url === route;
+  }
+
+  isRendered: boolean = false;
   toggleMenu() {
     this.isOpen = !this.isOpen;
+    setTimeout(() => {
+      this.isRendered = true;
+
+    }, 200);
+    this.isRendered = false;
+  }
+
+  ngOnInit() {
+
   }
 }
+
